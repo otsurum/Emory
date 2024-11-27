802cff8a22e98dde65d4d8b8bf3aa33b71419aa1
Crane-tsuru <gr26eate@i.softbank.jp>
2024-11-27 20:12:48 +0900

    .gitignoreを反映

---
+++ Emory/Emory/EmoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmoryApp.swift
+//  Emory
+//
+//  Created by 鶴見駿 on 2024/11/27.
+//
+
+import SwiftUI
+
+@main
+struct EmoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
