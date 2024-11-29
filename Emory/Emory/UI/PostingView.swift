355a635e6db8f8ebd2dc6de46b1bec025d2c23c2
Crane-tsuru <gr26eate@i.softbank.jp>
2024-11-29 20:08:24 +0900

    resize image in PostingView

--- Emory/Emory/UI/PostingView.swift
+++ Emory/Emory/UI/PostingView.swift
@@ -18,71 +18,78 @@ struct PostingView: View {
     
     
     var body: some View {
-        VStack(alignment: .leading) {
-            HStack {
-                TextField("タイトル", text: $inputTitle)
-                    .frame(width: 200, height: 50)
-                    .font(.system(size: fontSizeOnText, weight: .bold))
-//                    .textFieldStyle(.roundedBorder)
-//                    .background(Color.gray.opacity(0.2))い
+        GeometryReader { geometry in
+            VStack(alignment: .leading) {
+                HStack {
+                    TextField("タイトル", text: $inputTitle)
+                        .frame(width: 200, height: 50)
+                        .font(.system(size: fontSizeOnText, weight: .bold))
+    //                    .textFieldStyle(.roundedBorder)
+    //                    .background(Color.gray.opacity(0.2))い
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 8)
+                                .stroke(Color.gray, lineWidth: 2)
+                        )
+                        .padding()
+                    
+                    Spacer()
+                }
+                
+                RoundedRectangle(cornerRadius: 2)
+                    .stroke(lineWidth: 1)
+                    .frame(height: 50)
                     .overlay(
-                        RoundedRectangle(cornerRadius: 8)
-                            .stroke(Color.gray, lineWidth: 2)
+                        Text("タグを選択")
                     )
                     .padding()
                 
+                if let image {
+                    Image(uiImage: image)
+                        .resizable(resizingMode: .stretch)
+                        .aspectRatio(contentMode: .fit)
+                        .clipped()
+                        .padding()
+                    
+                    
+                    
+                } else {
+                    Button(action: {
+                        isPushedCamera = true
+                    }, label: {
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(lineWidth: 1.0)
+                            .frame(height: 300)
+                            .padding()
+                            .foregroundColor(.black)
+                            .overlay(
+                                Image(systemName: "camera")
+                                    .resizable()
+                                    .scaleEffect(1/2)
+                            )
+                            
+                    })
+                }
+                
                 Spacer()
             }
-            
-            RoundedRectangle(cornerRadius: 2)
-                .stroke(lineWidth: 1)
-                .frame(height: 50)
-                .overlay(
-                    Text("タグを選択")
-                )
-                .padding()
-            
-            if let image {
-                Image(uiImage: image)
-                    .resizable()
-                
-            } else {
-                Button(action: {
-                    isPushedCamera = true
-                }, label: {
-                    RoundedRectangle(cornerRadius: 10)
-                        .stroke(lineWidth: 1.0)
-                        .frame(height: 300)
-                        .padding()
-                        .foregroundColor(.black)
-                        .overlay(
-                            Image(systemName: "camera")
-                                .resizable()
-                                .scaleEffect(1/2)
-                        )
-                        
-                })
+            .fullScreenCover(isPresented: $isTakingImage) {
+                CameraCaptureView(image: $image)
+                    .ignoresSafeArea()
             }
-            
-            Spacer()
+            .popover(isPresented: $isSelectingImage) {
+                PhotoLibraryPickerView(image: $image)
+            }
+            .confirmationDialog("", isPresented: $isPushedCamera) {
+                Button("撮影") {
+                    isTakingImage = true
+                }
+                Button("ライブラリ") {
+                    isSelectingImage = true
+                }
+                Button("閉じる", role: .cancel) {
+                    isPushedCamera = false
+                }
         }
-        .fullScreenCover(isPresented: $isTakingImage) {
-            CameraCaptureView(image: $image)
-                .ignoresSafeArea()
-        }
-        .popover(isPresented: $isSelectingImage) {
-            PhotoLibraryPickerView(image: $image)
-        }
-        .confirmationDialog("", isPresented: $isPushedCamera) {
-            Button("撮影") {
-                isTakingImage = true
-            }
-            Button("ライブラリ") {
-                isSelectingImage = true
-            }
-            Button("閉じる", role: .cancel) {
-                isPushedCamera = false
-            }
         }
     }
 }
