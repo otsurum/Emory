3f5520645cfdb6fad4c2e58b4008268c933555cc
Crane-tsuru <gr26eate@i.softbank.jp>
2024-11-29 19:38:20 +0900

    PhotoLibraryとCameraの実装をするViewControllerを作成

---
+++ Emory/Emory/Camera/CameraCaptureView.swift
@@ -0,0 +1,52 @@
+//
+//  CameraCaptureView.swift
+//  Emory
+//
+//  Created by 鶴見駿 on 2024/11/29.
+//
+
+import SwiftUI
+
+struct CameraCaptureView: UIViewControllerRepresentable {
+    @Binding var image: UIImage?
+    @Environment(\.presentationMode) private var presentationMode
+    
+    
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.sourceType = .camera
+        picker.delegate = context.coordinator
+        picker.allowsEditing = true
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        let parent: CameraCaptureView
+        
+        init(_ parent: CameraCaptureView) {
+            self.parent = parent
+        }
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+            if let uiImage = info[.originalImage] as? UIImage {
+                parent.image = uiImage
+            }
+            
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+        
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+    }
+}
+
+//#Preview {
+//    CameraCaptureView()
+//}
