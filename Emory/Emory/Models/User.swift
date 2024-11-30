c3fa6d1cedc2528cad1134b1166ca5cf95549211
Crane-tsuru <gr26eate@i.softbank.jp>
2024-12-01 08:40:28 +0900

    define User properties

--- Emory/Emory/Models/User.swift
+++ Emory/Emory/Models/User.swift
@@ -8,6 +8,22 @@ import SwiftUI
 import SwiftData
 
 final class User {
-//    var name: String
+    let uuid = UUID().uuidString
+    var userName: String
+    var mailaddress: String
+    var FollowingList: [User]
+    var FollowerList: [User]
+    var likedPostList: [Post]
+    var repostedList: [Post]
+    var userIcon: UIImage?
     
+    init(userName: String, mailaddress: String, FollowingList: [User], FollowerList: [User], likedPostList: [Post], repostedList: [Post], userIcon: UIImage? = nil) {
+        self.userName = userName
+        self.mailaddress = mailaddress
+        self.FollowingList = FollowingList
+        self.FollowerList = FollowerList
+        self.likedPostList = likedPostList
+        self.repostedList = repostedList
+        self.userIcon = userIcon
+    }
 }
