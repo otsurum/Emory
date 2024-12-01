05612da68c5c75ac3f10b815479c338d238172d9
Crane-tsuru <gr26eate@i.softbank.jp>
2024-12-01 16:46:10 +0900

    UserのmailaddressプロパティをmailAddressに変更

--- Emory/Emory/Models/User.swift
+++ Emory/Emory/Models/User.swift
@@ -10,20 +10,28 @@ import SwiftData
 final class User {
     private let uuid = UUID().uuidString
     private var userName: String
-    private var mailaddress: String
+    private var mailAddress: String
     private var FollowingList: [User]
     private var FollowerList: [User]
     private var likedPostList: [Post]
     private var repostedList: [Post]
     private var userIcon: UIImage?
     
-    init(userName: String, mailaddress: String, FollowingList: [User], FollowerList: [User], likedPostList: [Post], repostedList: [Post], userIcon: UIImage? = nil) {
+    init(userName: String, mailAddress: String, FollowingList: [User], FollowerList: [User], likedPostList: [Post], repostedList: [Post], userIcon: UIImage? = nil) {
         self.userName = userName
-        self.mailaddress = mailaddress
+        self.mailAddress = mailAddress
         self.FollowingList = FollowingList
         self.FollowerList = FollowerList
         self.likedPostList = likedPostList
         self.repostedList = repostedList
         self.userIcon = userIcon
+    }
+    
+    func getUserName() -> String {
+        userName
+    }
+    
+    func getMailAddress() -> String {
+        mailAddress
     }
 }
