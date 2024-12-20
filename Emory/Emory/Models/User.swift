6db44fa153bbc6ebcbd6d8b6d7228d70c9bec44c
Crane-tsuru <gr26eate@i.softbank.jp>
2024-12-20 15:09:17 +0900

    UIIMageをData型に変換する関数をextensionで記述

--- Emory/Emory/Models/User.swift
+++ Emory/Emory/Models/User.swift
@@ -12,20 +12,20 @@ final class User {
     private var uuid = UUID().uuidString
     private var userName: String
     private var mailAddress: String
-    private var followingUserList: [User]
-    private var followerList: [User]
-    private var likedPostList: [Post]
-    private var repostedList: [Post]
-    private var userIcon: UIImage?
+//    private var followingUserList: [User]
+//    private var followerList: [User]
+//    private var likedPostList: [Post]
+//    private var repostedList: [Post]
+//    private var userIcon: UIImage?
     
     init(userName: String, mailAddress: String, followingUserList: [User], followerList: [User], likedPostList: [Post], repostedList: [Post], userIcon: UIImage? = nil) {
         self.userName = userName
         self.mailAddress = mailAddress
-        self.followingUserList = followingUserList
-        self.followerList = followerList
-        self.likedPostList = likedPostList
-        self.repostedList = repostedList
-        self.userIcon = userIcon
+//        self.followingUserList = followingUserList
+//        self.followerList = followerList
+//        self.likedPostList = likedPostList
+//        self.repostedList = repostedList
+//        self.userIcon = userIcon
     }
 }
 
@@ -38,25 +38,25 @@ extension User {
         mailAddress
     }
     
-    func getFollowingUserList() -> [User] {
-        followingUserList
-    }
-    
-    func getFollowerList() -> [User] {
-        followerList
-    }
-    
-    func getLikedPostList() -> [Post] {
-        likedPostList
-    }
-    
-    func getRepostedList() -> [Post] {
-        repostedList
-    }
-    
-    func getUserIcon() -> UIImage? {
-        userIcon
-    }
+//    func getFollowingUserList() -> [User] {
+//        followingUserList
+//    }
+//    
+//    func getFollowerList() -> [User] {
+//        followerList
+//    }
+//    
+//    func getLikedPostList() -> [Post] {
+//        likedPostList
+//    }
+//    
+//    func getRepostedList() -> [Post] {
+//        repostedList
+//    }
+//    
+//    func getUserIcon() -> UIImage? {
+//        userIcon
+//    }
     
     static func getExampleUser() -> User {
         User(userName: "あきら", mailAddress: "none", followingUserList: [], followerList: [], likedPostList: [], repostedList: [])
